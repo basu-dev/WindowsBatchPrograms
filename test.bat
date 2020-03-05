eea8470d6a240a8ba045ba0a8daa5d66c23ccbe8
basu-dev <adbasudev54@gmail.com>
2020-03-05 10:07:12 +0545

    first commit

--- test.bat
+++ test.bat
@@ -10,20 +10,22 @@ Rem echo Today is Thursday
 Rem )
 
 :day1
-Set /p task=Study Physics
-exit
+Set  task=Study Physics
+
 :day2
-Set /p task=Study Math
+Set  task=Study Math
+
 :day3
-Set /p task=Study Nep
+Set  task=Study Nep
 :day4
-Set /p task=Study Nep
+Set  task=Study Nep
 :day5
-Set /p task=Study What ,Set /p day=Sun
+Set  task=Study What ,Set /p day=Sun
 :day6
-Set /p task=Study The
+Set  task=Study The
 :day7
-Set /p task=Its Saturday
+Set  task=Its Saturday
+
 
 @For /F "tokens=2,3,4 delims=/ " %%A in ('Date /t') do @( 
     Set Month=%%A 
@@ -31,12 +33,12 @@ Set /p task=Its Saturday
     Set Year=%%C
    
 )
-echo %Day%
+
 if %Day%==01 goto day1
 if %Day%==02 goto day2
 if %Day%==03 goto day3
 if %Day%==04 goto day4
-if %Day%==05 goto day5
+if %Day%==05 (goto day5)
 if %Day%==06 goto day6
 if %Day%==07 goto day7 
 
